7a8fea60d89fc89bd5eda7ca291dcd8356b87c9f
ZEndOf2016 <37525441+ZEndOf1996@users.noreply.github.com>
2018-03-31 14:44:18 +0800

    1.增加了重置密码界面

---
+++ app/src/main/java/com/baluche/ResetPasswordActivity.java
@@ -0,0 +1,13 @@
+package com.baluche;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ResetPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.reset_password);
+    }
+}
